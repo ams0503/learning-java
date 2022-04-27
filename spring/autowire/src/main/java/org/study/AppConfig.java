b48202960e534cd1e465f5cf08985e8a38b695d9
ams0503 <babayaga.ams@gmail.com>
2022-04-27 14:15:47 +0700

    autowired

---
+++ spring/autowire/src/main/java/org/study/AppConfig.java
@@ -0,0 +1,10 @@
+package org.study;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("org.study")
+public class AppConfig {
+	
+}
---
+++ spring/autowire/src/main/java/org/study/interfaces/Engine.java
@@ -0,0 +1,8 @@
+package org.study.interfaces;
+
+import org.springframework.stereotype.Component;
+
+
+public interface Engine {
+	public String type(); 
+}
